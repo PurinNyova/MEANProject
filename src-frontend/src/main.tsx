c19e28377cfa5b44781e69ddbc7d55c85dfb972f
BenJeMic <FionaSherleen2005@outlook.com>
2025-01-23 20:50:57 +0700

    More frontend changes, navbar

--- src-frontend/src/main.tsx
+++ src-frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import { defaultSystem } from "@chakra-ui/react"
+import { BrowserRouter } from 'react-router-dom'
+import { ColorModeProvider } from './components/ui/color-mode.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider>
+          <App />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
